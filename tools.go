f7dd4511d1f9fec9740096cb10a5eaac42d15a35
hezhizhen <hezhizhen.yi@gmail.com>
2018-09-08 16:40:44 +0800

    more precise

--- tools.go
+++ tools.go
@@ -3,10 +3,11 @@ package tools
 // Days calculates total days given initial number and frequency
 func Days(init, freq int) int {
 	ret := 0
-	remain := init
-	for remain != 0 {
-		ret += remain
-		remain /= freq
+	articles := float64(init)
+	for articles >= 1 {
+		ret += int(articles)                // newly added articles which are actual
+		articles = articles/float64(freq) + // newly added articles in the round, including actual ones and potential ones
+			articles - float64(int(articles)) // potential articles in the last round
 	}
 	return ret
 }
---
+++ tools_test.go
@@ -0,0 +1,12 @@
+package tools_test
+
+import (
+	"fmt"
+	"testing"
+
+	tools "github.com/hezhizhen/tiny-tools"
+)
+
+func TestDays(t *testing.T) {
+	fmt.Println(tools.Days(41, 7))
+}
