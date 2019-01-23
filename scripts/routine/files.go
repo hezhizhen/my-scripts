cb29c54a3ed8c38c27dd1890ad8ed1ebf556d9b6
hezhizhen <hezhizhen.yi@gmail.com>
2019-01-23 20:37:51 +0800

    add routine

---
+++ scripts/routine/files.go
@@ -0,0 +1,41 @@
+package main
+
+// files is a map from categories to their relevant notes (done and to do)
+var files = map[string][]fileInfo{
+	"coursera": []fileInfo{
+		{
+			FileName: "15457126390831.md",
+			Title:    "《新教伦理与资本主义精神》导读",
+			Done:     false,
+		},
+	},
+	"book": []fileInfo{
+		{
+			FileName: "15469501270291.md",
+			Title:    "Go by Example",
+			Done:     false,
+		},
+	},
+	"clippings": []fileInfo{
+		{
+			FileName: "15473875670445.md",
+			Title:    "白夜行",
+			Done:     false,
+		},
+	},
+	"morningroutine": []fileInfo{
+		{
+			FileName: "15468295067722.md",
+			Title:    "My Morning Routine (Collection)",
+			Done:     false,
+		},
+	},
+	"wishlist": []fileInfo{
+		{
+			FileName: "15482192105949.md",
+			Title:    "WishList",
+			Done:     false,
+		},
+	},
+	"podcast": []fileInfo{},
+}
---
+++ scripts/routine/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/hezhizhen/tiny-tools/utilz"
+)
+
+type fileInfo struct {
+	FileName string
+	Title    string
+	Done     bool
+}
+
+func retrieveCategoriesAndFirstNotDoneFile() map[string]fileInfo {
+	ret := map[string]fileInfo{}
+	for category, fs := range files {
+		for _, f := range fs {
+			if f.Done {
+				continue
+			}
+			ret[category] = f
+			break
+		}
+	}
+	return ret
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	fileMap := retrieveCategoriesAndFirstNotDoneFile()
+	// check the number of arguments
+	switch len(args) {
+	case 0:
+		// help
+		fmt.Println("Available arguments:")
+		for category, f := range fileMap {
+			fmt.Printf("\t%-18s%s\n", category, f.Title) // 18 is the supposed maximum length of categories
+		}
+		os.Exit(0)
+	case 1:
+		break
+	default:
+		fmt.Printf("Multiple arguments (%s). Only one argument is required.\n", args)
+		os.Exit(1)
+	}
+	// check if arguemnt is correct
+	if _, exist := fileMap[args[0]]; !exist {
+		panic(fmt.Sprintf("Unknown category: %s", args[0]))
+	}
+	// set file path
+	notePath := "/Users/hezhizhen/Dropbox/MWeb3/docs/" + fileMap[args[0]].FileName
+	// open it with macvim
+	cmd := exec.Command("mvim", notePath)
+	utilz.Check(cmd.Run())
+}
